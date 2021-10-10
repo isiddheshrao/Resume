e63486a0361e621070c46f00746baacc566f059d
Siddhesh Rao <47262015+isiddheshrao@users.noreply.github.com>
2021-10-10 14:16:34 -0700

    Resume Updates

--- js/tag.js
+++
@@ -1,6 +0,0 @@
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-58582729-7"></script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-58582729-7');
